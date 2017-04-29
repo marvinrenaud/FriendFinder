d582fe0b924270e9920fb4deba3f398d488835d2
M R <M R>
2017-04-28 21:58:29 -0400

    wip: locally working version

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,6 +13,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
+  app.get("/api/friends", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../data/friends.js"));
+  });
+
   // If no matching route is found default to home
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
