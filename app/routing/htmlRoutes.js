aadeb9d118a5d69a508a09c643703741887090fd
M R <M R>
2017-04-29 09:49:46 -0400

    heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,19 +6,19 @@ var path = require('path');
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'home.html'));
+    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 
   app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'survey.html'));
+    res.sendFile(path.join(__dirname, 'survey.html'));
   });
 
   app.get('/api/friends', function(req, res) {
-    res.sendFile(path.join(__dirname, '../data','friends.js'));
+    res.sendFile(path.join(__dirname, '../data/friends.js'));
   });
 
   // If no matching route is found default to home
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '../public','home.html'));
+    res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 };
