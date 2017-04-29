c0398133bd6fd3e25e87c1d4a597ed4391d13103
M R <M R>
2017-04-29 09:44:39 -0400

    heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,24 +1,24 @@
 // DEPENDENCIES
-var path = require("path");
+var path = require('path');
 
 
 // ROUTING
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public", "home.html"));
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public', 'home.html'));
   });
 
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public", "survey.html"));
+  app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public', 'survey.html'));
   });
 
-  app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "../data","friends.js"));
+  app.get('/api/friends', function(req, res) {
+    res.sendFile(path.join(__dirname, '../data','friends.js'));
   });
 
   // If no matching route is found default to home
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../public","home.html"));
+    res.sendFile(path.join(__dirname, '../public','home.html'));
   });
 };
